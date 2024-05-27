30b143936085deffb501dc91ca1006d1481b0d21
HiDiHo <96914811+HiDiHo01@users.noreply.github.com>
2024-05-27 19:08:00 +0200

    Update __init__.py
    Update to latest v3.0.1

--- custom_components/frank_energie/__init__.py
+++ custom_components/frank_energie/__init__.py
@@ -1,16 +1,23 @@
 """The Frank Energie component."""
+# __init__.py
 
+import logging
 from typing import Any, Optional
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_TOKEN, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.typing import ConfigType
 from python_frank_energie import FrankEnergie
 
-from .const import _LOGGER, CONF_COORDINATOR, DOMAIN
+from .const import CONF_COORDINATOR, DOMAIN
 from .coordinator import FrankEnergieCoordinator
+from .exceptions import NoSuitableSitesFoundError
+
+_LOGGER = logging.getLogger(__name__)
 
 # PLATFORMS = [Platform.SENSOR, "frank_energie_diagnostic_sensor"]
 PLATFORMS = [Platform.SENSOR]
@@ -18,13 +25,20 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the Frank Energie component from a config entry."""
+    _LOGGER.debug("Setting up Frank Energie component for entry: %s", entry.entry_id)
     component = FrankEnergieComponent(hass, entry)
     return await component.setup()
-    # return True
 
 
 async def async_setup_platform(hass: HomeAssistant, config: dict[str, Any], async_add_entities, discovery_info=None):
     """Set up the Frank Energie sensor platform."""
+    _LOGGER.debug("Setting up Frank Energie sensor platform")
+    timezone = hass.config.time_zone
+    _LOGGER.info("Configured Time Zone: %s", timezone)
+    # You can pass the timezone to a platform if needed
+    hass.data[DOMAIN] = {
+        "timezone": timezone,
+    }
     coordinator = hass.data[DOMAIN][CONF_COORDINATOR]
     api = coordinator.api
     sensor = FrankEnergieDiagnosticSensor(api)
@@ -34,7 +48,7 @@ async def async_setup_platform(hass: HomeAssistant, config: dict[str, Any], asyn
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
-    _LOGGER.debug("Unloading entry...")
+    _LOGGER.debug("Unloading entry: %s", entry.entry_id)
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
@@ -50,33 +64,85 @@ class FrankEnergieComponent:
 
     async def setup(self) -> bool:
         """Set up the Frank Energie component from a config entry."""
+        _LOGGER.debug("Setting up Frank Energie component")
 
         # For backwards compatibility, set unique ID
         self._update_unique_id()
+
+        # Create API and Coordinator
         api = self._create_frank_energie_api()
         coordinator = self._create_frank_energie_coordinator(api)
-        # await coordinator.async_config_entry_first_refresh()
-        await self._async_config_entry_first_refresh(coordinator)
-        self._save_coordinator_to_hass_data(coordinator)
-        # await self.hass.config_entries.async_forward_entry_setups(self.entry, PLATFORMS)
-        await self._async_forward_entry_setups()
-        return True
 
-    async def _async_config_entry_first_refresh(self, coordinator: FrankEnergieCoordinator) -> None:
-        """Perform the initial refresh for the coordinator."""
+        # Awaiting the coroutine method call
+        await self._select_site_reference(coordinator)
+
+        # Perform the initial refresh for the coordinator
+        _LOGGER.debug("Performing initial refresh for coordinator")
         await coordinator.async_config_entry_first_refresh()
 
-    async def _async_forward_entry_setups(self) -> None:
-        """Forward entry setups to appropriate platforms."""
-        await self.hass.config_entries.async_forward_entry_setups(self.entry, PLATFORMS)
+        # Save the coordinator to Home Assistant data
+        await self._save_coordinator_to_hass_data(coordinator)
+
+        # Forward entry setups to appropriate platforms
+        _LOGGER.debug("Forwarding entry setups to platforms")
+        await self._async_forward_entry_setups()
+
+        return True
 
     def _update_unique_id(self) -> None:
         """Update the unique ID of the config entry."""
         if self.entry.unique_id is None or self.entry.unique_id == "frank_energie_component":
             self.hass.config_entries.async_update_entry(self.entry, unique_id="frank_energie")
 
+    def fix_update_unique_id(self) -> None:
+        """Update the unique ID of the config entry."""
+        if self.entry.unique_id is None or self.entry.unique_id == "frank_energie_component":
+            self.hass.config_entries.update_entry(self.entry, unique_id="frank_energie")
+
+    async def _select_site_reference(self, coordinator: FrankEnergieCoordinator) -> None:
+        """Select the site reference for the coordinator."""
+        _LOGGER.debug("Selecting site reference for coordinator")
+        if self.entry.data.get("site_reference") is None and self.entry.data.get(CONF_ACCESS_TOKEN):
+            site_reference, title = await self._get_site_reference_and_title(coordinator)
+            if not site_reference:
+                raise NoSuitableSitesFoundError("No suitable sites found for this account")
+
+            # Controleer of de titel correct is gegenereerd
+            if not isinstance(title, str):
+                _LOGGER.warning("Failed to generate title for the site reference: %s", site_reference)
+                return
+
+            _LOGGER.debug("Site reference: %s, Title: %s", site_reference, title)
+            # Update entry data and title using async_update_entry method
+            self.hass.config_entries.async_update_entry(self.entry, data={**self.entry.data, "site_reference": site_reference}, title=title)
+
+    async def _get_site_reference_and_title(self, coordinator: FrankEnergieCoordinator) -> tuple[str, str]:
+        _LOGGER.debug("Getting site reference and title for coordinator")
+
+        # Haal de 'Me' gegevens op van de coordinator API
+        me_data = await coordinator.api.me()
+
+        # Haal de bezorgsites op uit de 'Me' gegevens
+        delivery_sites = me_data.deliverySites
+
+        # Controleer of er bezorgsites zijn gevonden
+        if not delivery_sites:
+            raise NoSuitableSitesFoundError("No suitable delivery sites found for this account")
+
+        # Selecteer de eerste bezorgsite voor nu, je kunt logica toevoegen om de juiste site te selecteren op basis van voorkeuren
+        selected_site = delivery_sites[0]
+
+        # Maak een titel op basis van de adresgegevens van de bezorgsite
+        title = f"{selected_site.address.street} {selected_site.address.houseNumber}"
+        if selected_site.address.houseNumberAddition:
+            title += f" {selected_site.address.houseNumberAddition}"
+
+        # Retourneer de referentie van de site en de titel
+        return selected_site.reference, title
+
     def _create_frank_energie_api(self) -> FrankEnergie:
         """Create the Frank Energie API instance."""
+        _LOGGER.debug("Creating Frank Energie API instance")
         return FrankEnergie(
             clientsession=async_get_clientsession(self.hass),
             auth_token=self.entry.data.get(CONF_ACCESS_TOKEN),
@@ -85,17 +151,24 @@ class FrankEnergieComponent:
 
     def _create_frank_energie_coordinator(self, api: FrankEnergie) -> FrankEnergieCoordinator:
         """Create the Frank Energie Coordinator instance."""
+        _LOGGER.debug("Creating Frank Energie Coordinator instance")
         return FrankEnergieCoordinator(self.hass, self.entry, api)
+        # return FrankEnergieCoordinator(self.hass, self.entry, api, self.entry.data.get("site_reference"))
 
-    def _save_coordinator_to_hass_data(self, coordinator: FrankEnergieCoordinator) -> None:
+    async def _async_forward_entry_setups(self) -> None:
+        """Forward entry setups to appropriate platforms."""
+        _LOGGER.debug("Forwarding entry setups to platforms")
+        await self.hass.config_entries.async_forward_entry_setups(self.entry, PLATFORMS)
+
+    async def _save_coordinator_to_hass_data(self, coordinator: FrankEnergieCoordinator) -> None:
         """Save the coordinator to the Home Assistant data."""
-        # self.hass.data.setdefault(DOMAIN, {})
-        # self.hass.data[DOMAIN][self.entry.entry_id] = {CONF_COORDINATOR: coordinator}
+        _LOGGER.debug("Saving coordinator to Home Assistant data")
         hass_data = self.hass.data.setdefault(DOMAIN, {})
         hass_data[self.entry.entry_id] = {CONF_COORDINATOR: coordinator}
 
     def _remove_entry_from_hass_data(self) -> None:
         """Remove the entry from the Home Assistant data."""
+        _LOGGER.debug("Removing entry from Home Assistant data")
         self.hass.data[DOMAIN].pop(self.entry.entry_id)
 
 
@@ -126,6 +199,7 @@ class FrankEnergieDiagnosticSensor(Entity):
 
     async def async_update(self) -> None:
         """Update the sensor state."""
+        _LOGGER.debug("Updating FrankEnergieDiagnosticSensor")
         # Implement the logic to update the sensor state
         # You can use the FrankEnergie API client instance (self._frank_energie)
         # to fetch diagnostic data and update the sensor state accordingly
@@ -133,4 +207,4 @@ class FrankEnergieDiagnosticSensor(Entity):
             self._state = await self._frank_energie.get_diagnostic_data()
         except Exception as e:
             # Handle specific exceptions and raise more descriptive ones if necessary
-            raise ValueError(f"Failed to update FrankEnergieDiagnosticSensor: {str(e)}")
+            raise ValueError(f"Failed to update FrankEnergieDiagnosticSensor: {str(e)}") from e
