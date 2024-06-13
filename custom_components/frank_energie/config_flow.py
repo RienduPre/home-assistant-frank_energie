e1f2d6f5c0c4e8075c7e54e7baf5db5c8685ddf5
HiDiHo <96914811+HiDiHo01@users.noreply.github.com>
2024-06-14 01:47:41 +0200

    Update config_flow.py
    added site selection
    update 14-6-2024

--- custom_components/frank_energie/config_flow.py
+++ custom_components/frank_energie/config_flow.py
@@ -1,26 +1,30 @@
 """Config flow for Frank Energie integration."""
+# config_flow.py
 import logging
 from collections.abc import Mapping
 from typing import Any, Optional
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.const import (
-    CONF_ACCESS_TOKEN,
-    CONF_AUTHENTICATION,
-    CONF_PASSWORD,
-    CONF_TOKEN,
-    CONF_USERNAME,
-)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (CONF_ACCESS_TOKEN, CONF_AUTHENTICATION,
+                                 CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME)
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
-from python_frank_energie import FrankEnergie
-from python_frank_energie.exceptions import AuthException
+from homeassistant.helpers.selector import (SelectSelector,
+                                            SelectSelectorConfig,
+                                            SelectSelectorMode)
+from python_frank_energie import Authentication, FrankEnergie
+from python_frank_energie.exceptions import AuthException, ConnectionException
 
-from .const import DOMAIN
-from .api import FrankEnergieAPI
+from .const import CONF_SITE, DOMAIN
 
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+_LOGGER: logging.Logger = logging.getLogger(__name__)
+
+
+async def async_handle_auth_failure(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle an authentication failure by triggering reauthentication."""
+    hass.config_entries.async_start_reauth(entry.entry_id)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -32,147 +36,112 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Initialize the config flow."""
         self._errors: dict[str, str] = {}
         self._reauth_entry: Optional[config_entries.ConfigEntry] = None
+        self.sign_in_data: dict[str, Any] = {}
 
-    async def async_step_login(
-        self, user_input: Optional[dict[str, Any]] = None, errors=None
-    ) -> FlowResult:
+    async def async_step_login(self, user_input: Optional[dict[str, Any]] = None, errors: Optional[dict[str, str]] = None) -> FlowResult:
         """Handle login with credentials by user."""
         if not user_input:
-            username = (
-                self._reauth_entry.data[CONF_USERNAME]
-                if self._reauth_entry
-                else None
-            )
-
-            data_schema = vol.Schema(
-                {
-                    vol.Required(CONF_USERNAME, default=username): vol.Coerce(str),
-                    vol.Required(CONF_PASSWORD): vol.Coerce(str),
-                }
-            )
-
-            return self.async_show_form(
-                step_id="login",
-                data_schema=data_schema,
-                errors=errors,
-            )
+            return self._show_login_form()
 
         errors = self._validate_login_input(user_input)
 
         if errors:
             return self._show_login_form(errors=errors)
 
-        # Create an instance of the FrankEnergieAPI class
-        # api = FrankEnergie()
-        async with FrankEnergie() as api:
-            try:
-                # Authenticate with Frank Energie API
-                auth = await api.login(
-                        user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
-                    )
-            except AuthException as ex:
-                    # _LOGGER.exception("Error during login")
-                    _LOGGER.exception("Error during login", exc_info=ex)
-                    return await self.async_step_login(errors={"base": "invalid_auth"})
-    
-            data = {
-                CONF_USERNAME: user_input[CONF_USERNAME],
-                CONF_ACCESS_TOKEN: auth.authToken,
-                CONF_TOKEN: auth.refreshToken,
-            }
-    
-            # Check if a refresh token is available
-            if self._reauth_entry and CONF_TOKEN in self._reauth_entry.data:
-                # Use the refresh token to get a new auth token
-                async with FrankEnergie() as api:
-                    try:
-                        # Save the credentials to the config entry's data
-                        self.hass.config_entries.async_update_entry(
-                            self._reauth_entry,
-                            data=data,
-                        )
-    
-                        self.hass.async_create_task(
-                            self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
-                        )
-    
-                        return self.async_abort(reason="reauth_successful")
-                    except AuthException as ex:
-                        _LOGGER.exception("Error during login", exc_info=ex)
-                        return await self.async_step_login(errors={"base": "invalid_auth"})
-            else:
-                # Perform the initial login
-                async with FrankEnergie() as api:
-                    try:
-                        auth = await api.login(
-                            user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
-                        )
-                    except AuthException as ex:
-                        _LOGGER.exception("Error during login", exc_info=ex)
-                        return await self.async_step_login(errors={"base": "invalid_auth"})
-    
-            data = {
-                CONF_USERNAME: user_input[CONF_USERNAME],
-                CONF_ACCESS_TOKEN: auth.authToken,
-                CONF_TOKEN: auth.refreshToken,
-            }
-    
-            if self._reauth_entry:
-                self.hass.config_entries.async_update_entry(
-                    self._reauth_entry,
-                    data=data,
-                )
-    
-                self.hass.async_create_task(
-                    self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
-                )
-    
-                return self.async_abort(reason="reauth_successful")
-    
-            await self.async_set_unique_id(user_input[CONF_USERNAME])
-            self._abort_if_unique_id_configured()
-    
-            return await self._async_create_entry(data)
+        auth = await self._authenticate(user_input)
+        if auth:
+            return await self._handle_authentication_success(user_input, auth)
+        else:
+            return await self._handle_authentication_failure()
 
-    async def async_step_user(
-        self, user_input=None, errors=None
-    ) -> FlowResult:
+    async def async_step_site(self, user_input=None, errors=None) -> FlowResult:
+        """Handle possible multi site accounts."""
+        if user_input and user_input.get(CONF_SITE) is not None:
+            self.sign_in_data[CONF_SITE] = user_input[CONF_SITE]
+            return await self._async_create_entry(self.sign_in_data)
+
+        try:
+            api = FrankEnergie(
+                auth_token=self.sign_in_data.get(CONF_ACCESS_TOKEN, None),
+                refresh_token=self.sign_in_data.get(CONF_TOKEN, None),
+            )
+            me = await api.me()
+
+            all_delivery_sites = me.deliverySites
+
+            # filter out all sites that are not in delivery
+            # me.deliverySites = [site for site in all_delivery_sites if site.status == "IN_DELIVERY"]
+            in_delivery_sites = [site for site in all_delivery_sites if site.status == "IN_DELIVERY"]
+
+            number_of_sites = len(in_delivery_sites)
+
+            if not in_delivery_sites:
+                raise Exception("No suitable sites found for this account")
+
+            if number_of_sites > 0:
+                first_site = in_delivery_sites[0]
+
+            if number_of_sites == 1:
+                # for backward compatibility (do nothing)
+                # Check if entry with CONF_USERNAME exists, then abort
+                if CONF_USERNAME in user_input:
+                    await self.async_set_unique_id(user_input[CONF_USERNAME])
+                    self._abort_if_unique_id_configured()
+
+                # Create entry with unique_id as me.deliverySites[0].reference
+                self.sign_in_data[CONF_SITE] = first_site.reference
+                self.sign_in_data[CONF_USERNAME] = self.create_title(first_site)
+                return await self._async_create_entry(self.sign_in_data)
+
+            # Prepare site options for selection
+            site_options = [{"value": site.reference, "label": self.create_title(site)} for site in me.deliverySites]
+            default_site = first_site.reference
+
+            options = {
+                vol.Required(CONF_SITE, default=default_site): SelectSelector(
+                    SelectSelectorConfig(
+                        options=site_options,
+                        mode=SelectSelectorMode.LIST,
+                    )
+                )
+            }
+
+            return self.async_show_form(
+                step_id="site", data_schema=vol.Schema(options), errors=errors
+            )
+
+        except AuthException:
+            return self.async_show_form(
+                step_id="login",
+                data_schema=data_schema,
+                errors={"base": "invalid_auth"},
+            )
+        except ConnectionException:
+            return self.async_show_form(
+                step_id="login",
+                data_schema=data_schema,
+                errors={"base": "connection_error"},
+            )
+
+    async def async_step_user(self, user_input: Optional[dict[str, Any]] = None) -> FlowResult:
         """Handle a flow initiated by the user."""
-        #if not user_input:
-        if user_input is None:
+        if not user_input:
             data_schema = vol.Schema(
                 {
-                    vol.Required(CONF_AUTHENTICATION, default=True): bool,
+                    vol.Required(CONF_AUTHENTICATION): bool,
                 }
             )
 
-            return self.async_show_form(
-                step_id="user",
-                data_schema=data_schema,
-                errors=errors,
-            )
+            return self.async_show_form(step_id="user", data_schema=data_schema)
 
         if user_input[CONF_AUTHENTICATION]:
             return await self.async_step_login()
 
-        data: dict[str, Any] = {}
+        data = {}
 
         return await self._async_create_entry(data)
 
-    async def _show_config_form(self) -> FlowResult:
-        """Show the configuration form to edit login information."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(CONF_USERNAME): str,
-                    vol.Optional(CONF_PASSWORD): str,
-                }
-            ),
-            errors=self._errors,
-        )
-
-    async def async_step_reauth(self) -> FlowResult:
+    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
         """Handle configuration by re-auth."""
         self._reauth_entry = self.hass.config_entries.async_get_entry(
             self.context["entry_id"]
@@ -180,45 +149,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return await self.async_step_login()
 
     async def _async_create_entry(self, data):
-        unique_id = data.get(CONF_USERNAME, "frank_energie")
+        # await self.async_set_unique_id(data.get(CONF_USERNAME, "frank_energie"))
+        unique_id = data[CONF_SITE] + data[CONF_USERNAME]
         await self.async_set_unique_id(unique_id)
         self._abort_if_unique_id_configured()
 
-        entry_data = {
-            CONF_USERNAME: data.get(CONF_USERNAME),
-            CONF_ACCESS_TOKEN: data.get(CONF_ACCESS_TOKEN),
-            CONF_TOKEN: data.get(CONF_TOKEN),
-        }
-
-        if self._reauth_entry:
-            self.hass.config_entries.async_update_entry(
-                self._reauth_entry, data=entry_data
-            )
-            self.hass.async_create_task(
-                self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
-            )
-            return self.async_abort(reason="reauth_successful")
-
-        if CONF_TOKEN in data:
-            entry_data[CONF_TOKEN] = data[CONF_TOKEN]
-
         return self.async_create_entry(
-            title=data.get(CONF_USERNAME, "Frank Energie"), data=entry_data
+            title=data.get(CONF_USERNAME, "Frank Energie"), data=data
         )
 
-    def _show_user_form(self, errors: Optional[dict[str, str]] = None) -> FlowResult:
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): str,
-                vol.Required(CONF_AUTHENTICATION, default=True): bool,
-            }
-        )
+    @staticmethod
+    def create_title(site) -> str:
+        title = f"{site.address.street} {site.address.houseNumber}"
+        if site.address.houseNumberAddition is not None:
+            title += f" {site.address.houseNumberAddition}"
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=data_schema,
-            errors=errors,
-        )
+        return title
 
     def _show_login_form(self, errors: Optional[dict[str, str]] = None) -> FlowResult:
         username = (
@@ -240,20 +186,58 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
+    async def _authenticate(self, user_input: dict[str, Any]) -> Optional[Authentication]:
+        """Authenticate with Frank Energie API."""
+        async with FrankEnergie() as api:
+            try:
+                return await api.login(user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
+            except AuthException as ex:
+                _LOGGER.exception("Error during login", exc_info=ex)
+                return None
+
+    async def _handle_authentication_success(self, user_input: dict[str, Any], auth: Authentication) -> FlowResult:
+        """Handle successful authentication."""
+        self.sign_in_data = {
+            CONF_USERNAME: user_input[CONF_USERNAME],
+            CONF_ACCESS_TOKEN: auth.authToken,
+            CONF_TOKEN: auth.refreshToken
+        }
+        if self._reauth_entry:
+            self.hass.config_entries.async_update_entry(
+                self._reauth_entry,
+                data=self.sign_in_data
+            )
+            self.hass.async_create_task(
+                self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
+            )
+            return self.async_abort(reason="reauth_successful")
+        else:
+            return await self.async_step_site(self.sign_in_data)
+
+    async def _handle_authentication_failure(self) -> FlowResult:
+        """Handle authentication failure."""
+        return await self.async_step_login(errors={"base": "invalid_auth"})
+
     @staticmethod
     @callback
     def async_get_options_flow(config_entry: config_entries.ConfigEntry):
+        """Get options flow handler."""
+        _LOGGER.debug("config_entry for %s", config_entry)
+        # if config_entry.unique_id != DOMAIN:
         return FrankEnergieOptionsFlowHandler(config_entry)
+        # return True
 
     @staticmethod
     def _validate_login_input(user_input: dict[str, Any]) -> dict[str, str]:
+        """Validate user input for login."""
         errors = {}
         if user_input[CONF_USERNAME].strip() == "":
             errors[CONF_USERNAME] = "Username is required."
         if user_input[CONF_PASSWORD].strip() == "":
             errors[CONF_PASSWORD] = "Password is required."
         return errors
-    
+
+
 class FrankEnergieOptionsFlowHandler(config_entries.OptionsFlow):
     """Frank Energie config flow options handler."""
 
@@ -266,7 +250,7 @@ class FrankEnergieOptionsFlowHandler(config_entries.OptionsFlow):
         """Manage the options."""
         return await self.async_step_user()
 
-    async def async_step_user(self, user_input=None, errors=None):
+    async def async_step_user(self, user_input: Optional[dict[str, Any]] = None, errors=None):
         """Handle a flow initialized by the user."""
         if user_input is not None:
             self.options.update(user_input)
@@ -278,21 +262,38 @@ class FrankEnergieOptionsFlowHandler(config_entries.OptionsFlow):
             else None
         )
 
+        data_schema = vol.Schema({
+            vol.Required(CONF_USERNAME, default=username): str,
+            vol.Required(CONF_PASSWORD): str
+        })
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_USERNAME, default=username): str,
-                    vol.Required(CONF_PASSWORD): str,
-                }
-            ),
-            errors=errors,
+            data_schema=data_schema,
+            errors=errors
         )
-
 
     async def _update_options(self):
         """Update config entry options."""
         return self.async_create_entry(
             title=self.config_entry.data.get(CONF_USERNAME),
-            data=self.options,
+            data=self.options
         )
+
+
+class NoOptionsFlowHandler(config_entries.OptionsFlow):
+    """Options flow handler for when unique_id matches DOMAIN."""
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None, errors=None):
+        """Handle a flow initiated by the user."""
+        # Implement specific handling for when options are not needed
+        pass
+
+    async def _update_options(self):
+        """Update config entry options."""
+        # Implement specific handling for when options are not needed
+        pass
