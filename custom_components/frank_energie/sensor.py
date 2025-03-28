57ced10deda5683103c6454fbc94851b3f490aff
Roy <96914811+HiDiHo01@users.noreply.github.com>
2025-03-28 17:17:34 +0100

    Tiny fix for sensor

--- custom_components/frank_energie/sensor.py
+++ custom_components/frank_energie/sensor.py
@@ -5,20 +5,12 @@ from dataclasses import dataclass, field
 from datetime import timedelta
 from typing import Any, Callable, Final, Optional, Union
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorEntityDescription,
-    SensorStateClass,
-)
+from homeassistant.components.sensor import (SensorDeviceClass, SensorEntity,
+                                             SensorEntityDescription,
+                                             SensorStateClass)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CURRENCY_EURO,
-    PERCENTAGE,
-    STATE_UNKNOWN,
-    UnitOfEnergy,
-    UnitOfVolume,
-)
+from homeassistant.const import (CURRENCY_EURO, PERCENTAGE, STATE_UNKNOWN,
+                                 UnitOfEnergy, UnitOfVolume)
 from homeassistant.core import HassJob, HomeAssistant
 from homeassistant.helpers import event
 from homeassistant.helpers.device_registry import DeviceEntryType
@@ -28,29 +20,12 @@ from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import dt, utcnow
 
-from .const import (
-    API_CONF_URL,
-    ATTR_TIME,
-    ATTRIBUTION,
-    COMPONENT_TITLE,
-    CONF_COORDINATOR,
-    DATA_ELECTRICITY,
-    DATA_GAS,
-    DATA_INVOICES,
-    DATA_MONTH_SUMMARY,
-    DATA_USAGE,
-    DATA_USER,
-    DATA_USER_SITES,
-    DOMAIN,
-    ICON,
-    SERVICE_NAME_COSTS,
-    SERVICE_NAME_PRICES,
-    SERVICE_NAME_USAGE,
-    SERVICE_NAME_USER,
-    UNIT_ELECTRICITY,
-    UNIT_GAS,
-    VERSION,
-)
+from .const import (API_CONF_URL, ATTR_TIME, ATTRIBUTION, COMPONENT_TITLE,
+                    CONF_COORDINATOR, DATA_ELECTRICITY, DATA_GAS,
+                    DATA_INVOICES, DATA_MONTH_SUMMARY, DATA_USAGE, DATA_USER,
+                    DATA_USER_SITES, DOMAIN, ICON, SERVICE_NAME_COSTS,
+                    SERVICE_NAME_PRICES, SERVICE_NAME_USAGE, SERVICE_NAME_USER,
+                    UNIT_ELECTRICITY, UNIT_GAS, VERSION)
 from .coordinator import FrankEnergieCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -1676,8 +1651,7 @@ SENSOR_TYPES: tuple[FrankEnergieEntityDescription, ...] = (
         service_name=SERVICE_NAME_USER,
         value_fn=lambda data: data[DATA_USER].UserSettings.get(
             "smartPushNotifications")
-        if data[DATA_USER].UserSettings.get(
-            "smartPushNotifications")
+        if data[DATA_USER].UserSettings
         else None
     ),
     FrankEnergieEntityDescription(
